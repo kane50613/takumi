f9daec14700a73f3ff27bb9bc160aac24c1100d5
Kane Wang <kane@yeecord.com>
2026-03-14 13:32:33 +0800

    fix clippy

--- takumi/benches/gradient.rs
+++ takumi/benches/gradient.rs
@@ -4,7 +4,7 @@ use takumi::{
   GlobalContext,
   layout::{
     Viewport,
-    node::{ContainerNode, Node, NodeKind},
+    node::{ContainerNode, NodeKind},
     style::{BackgroundImages, FromCss, Length, Style, StyleDeclaration},
   },
   rendering::{RenderOptionsBuilder, render},
