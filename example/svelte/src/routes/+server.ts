e229c4f0b0bb4cd35801d90149af065720ec1192
Kane Wang <kane@yeecord.com>
2026-03-24 23:20:27 +0800

    Example: update svelte to use vercel adapter

--- example/svelte/src/routes/+server.ts
+++ example/svelte/src/routes/+server.ts
@@ -1,11 +1,25 @@
 import { ImageResponse } from "@takumi-rs/image-response";
 
 export function GET() {
-  return new ImageResponse("Hello World", {
-    width: 1200,
-    height: 630,
-    headers: {
-      "cache-control": "no-cache, no-store, must-revalidate",
+  return new ImageResponse(
+    {
+      key: "root",
+      type: "div",
+      props: {
+        tw: "flex items-center justify-center w-full h-full from-cyan-50 to-sky-100 bg-gradient-to-br font-bold text-4xl text-slate-700",
+        children: "SvelteKit + Takumi",
+      },
     },
-  });
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Geist",
+          data: () =>
+            fetch("https://takumi.kane.tw/fonts/Geist.woff2").then((res) => res.arrayBuffer()),
+        },
+      ],
+    },
+  );
 }
