3476c7a89ca1a061eeb6b01e63e086c00759ee15
Kane Wang <kane@yeecord.com>
2026-04-07 20:40:59 +0800

    JS: replace wildcard type exports with explicit public API

--- takumi-js/src/index.ts
+++ takumi-js/src/index.ts
@@ -1,6 +1,32 @@
-export type * from "@takumi-rs/helpers";
-export type * from "@takumi-rs/core";
-export * from "./render";
+export type {
+  ContainerNode,
+  FetchResourcesOptions,
+  ImageNode,
+  Node,
+  NodeAttributes,
+  NodeMetadata,
+  ReactElementLike,
+  TextNode,
+} from "@takumi-rs/helpers";
+export type {
+  AnimationFrameSource,
+  AnimationOutputFormat,
+  AnimationSceneSource,
+  ConstructRendererOptions,
+  DitheringAlgorithm,
+  EncodeFramesOptions,
+  Font,
+  FontDetails,
+  ImageSource,
+  Keyframes,
+  KeyframesMap,
+  KeyframesRuleList,
+  MeasuredNode,
+  MeasuredTextRun,
+  OutputFormat,
+  RenderAnimationOptions,
+} from "@takumi-rs/core";
+export { render } from "./render";
 
 export type { RenderOptions } from "./render";
 
