832d092557d76bafcda550af95addfc9d0bcaee8
Kane Wang <kane@yeecord.com>
2026-04-24 01:10:40 +0800

    Crate: fix inflow

--- takumi/src/layout/tree.rs
+++ takumi/src/layout/tree.rs
@@ -898,12 +898,12 @@ impl<'g> RenderNode<'g> {
   }
 
   pub fn participates_as_inline_box(&self) -> bool {
-    self.is_inline_atomic_container()
+    self.is_inline_atomic_container() || self.context.style.float != Float::None
   }
 
   fn participates_in_inflow_inline_formatting_context(&self) -> bool {
     self.is_inline_level()
-      || self.participates_as_inline_box()
+      || self.is_inline_atomic_container()
       || self.anonymous_text_content.is_some()
   }
 
@@ -1125,10 +1125,10 @@ impl<'g> RenderNode<'g> {
         } else {
           let has_inline = children
             .iter()
-            .any(RenderNode::participates_in_inflow_inline_formatting_context);
+            .any(RenderNode::participates_in_inline_formatting_context);
           let has_block = children
             .iter()
-            .any(|child| !child.participates_in_inflow_inline_formatting_context());
+            .any(|child| !child.participates_in_inline_formatting_context());
           let requires_inline_parent_blockification =
             finished.context.style.display.is_inline() && has_block;
           let needs_anonymous_boxes = has_inline && has_block;
@@ -1151,7 +1151,7 @@ impl<'g> RenderNode<'g> {
             let mut inline_group = Vec::new();
 
             for item in children {
-              if item.participates_in_inflow_inline_formatting_context() {
+              if item.participates_in_inline_formatting_context() {
                 inline_group.push(item);
                 continue;
               }
