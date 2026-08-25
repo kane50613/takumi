8c30ab3d2037012846db78bd77caec792bfe6f87
Kane Wang <kane@yeecord.com>
2026-08-25 10:51:38 +0800

    Helpers: flatten nested palettes into cssVariables (#1405)

---
+++ takumi-helpers/tests/css-variables.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "bun:test";
+import { cssVariables } from "../src/css-variables";
+
+test("joins nested keys into one variable name", () => {
+  expect(cssVariables({ color: { brand: { 500: "#5b21b6" } }, spacing: "0.25rem" })).toEqual({
+    "--color-brand-500": "#5b21b6",
+    "--spacing": "0.25rem",
+  });
+});
+
+test("DEFAULT names the parent scale", () => {
+  expect(cssVariables({ color: { brand: { DEFAULT: "#5b21b6", 300: "#c4b5fd" } } })).toEqual({
+    "--color-brand": "#5b21b6",
+    "--color-brand-300": "#c4b5fd",
+  });
+});
+
+test("a root-level DEFAULT names nothing and is dropped", () => {
+  expect(cssVariables({ DEFAULT: "#5b21b6" })).toEqual({});
+});
+
+test("stringifies numeric leaves", () => {
+  expect(cssVariables({ leading: { tight: 1.25 } })).toEqual({ "--leading-tight": "1.25" });
+});
