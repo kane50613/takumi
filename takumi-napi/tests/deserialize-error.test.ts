3fa1ce979d6a5f8e33e19cf143a52239924a27ee
Kane Wang <kane@yeecord.com>
2026-09-11 21:43:35 +0800

    JS: list the sizing keywords in the width diagnostics

--- takumi-napi/tests/deserialize-error.test.ts
+++ takumi-napi/tests/deserialize-error.test.ts
@@ -146,7 +146,7 @@ test("report deserialize error for width property with invalid type", () => {
       ),
     "width",
     "boolean `true`",
-    "a value of <length>; also accepts 'initial', 'unset' or 'inherit'.",
+    "a value of <length>, 'min-content', 'max-content', 'fit-content', 'stretch' or <fit-content()>; also accepts 'initial', 'unset' or 'inherit'.",
   );
 });
 
@@ -169,7 +169,7 @@ test("report deserialize error for width property with invalid string value", ()
     "width",
     "invalid",
     "invalid",
-    "a value of <length>; also accepts 'initial', 'unset' or 'inherit'.",
+    "a value of <length>, 'min-content', 'max-content', 'fit-content', 'stretch' or <fit-content()>; also accepts 'initial', 'unset' or 'inherit'.",
   );
 });
 
